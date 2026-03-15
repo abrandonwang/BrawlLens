f78ef199a8abb062669efb39d9f9f2ac58888209
abrandonwang <abrandonwang@gmail.com>
2026-03-15 13:17:36 -0500

    outbound ip request

--- src/app/api/player/route.ts
+++ src/app/api/player/route.ts
@@ -4,6 +4,10 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const tag = searchParams.get('tag')
 
+    const ipRes = await fetch("https://api.ipify.org?format=json")
+    const { ip: serverIp } = await ipRes.json()
+    console.log("Outbound IP:", serverIp)
+
     const response = await fetch(`https://api.brawlstars.com/v1/players/%23${tag}`, {
         headers: {
             "Authorization": `Bearer ${process.env.BRAWL_API_KEY}`
