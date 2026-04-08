e57af559bd16b1da1d0ad8f8c3ae360865d09eee
abrandonwang <abrandonwang@gmail.com>
2026-04-07 23:21:46 -0500

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "../components/NavBar"
-import Footer from "../components/Footer"
-import { MenuProvider } from "../context/MenuContext"
-import { ThemeProvider } from "../components/ThemeProvider"
-import TopLoader from "../components/TopLoader"
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import { MenuProvider } from "../context/MenuContext";
+import { ThemeProvider } from "../components/ThemeProvider";
+import TopLoader from "../components/TopLoader";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -13,22 +14,30 @@ export const metadata: Metadata = {
   title: "BrawlLens - The Pro Library",
   description: "Advanced analytics for competitive players.",
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><circle cx=%2250%22 cy=%2250%22 r=%2240%22 fill=%22none%22 stroke=%22black%22 stroke-width=%2210%22/><circle cx=%2250%22 cy=%2250%22 r=%2210%22 fill=%22black%22/></svg>',
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><circle cx=%2250%22 cy=%2250%22 r=%2240%22 fill=%22none%22 stroke=%22black%22 stroke-width=%2210%22/><circle cx=%2250%22 cy=%2250%22 r=%2210%22 fill=%22black%22/></svg>",
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning className={`${inter.variable} flex flex-col min-h-dvh overflow-y-auto bg-[#f5f5f5] dark:bg-[#0a0a0a]`}>
+      <body
+        suppressHydrationWarning
+        className={`${inter.variable} flex flex-col min-h-dvh overflow-y-auto bg-[#f5f5f5] dark:bg-[#0a0a0a]`}
+      >
         <ThemeProvider>
           <MenuProvider>
             <TopLoader />
-            <NavBar/>
+            <NavBar />
             {children}
-            <Footer/>
+            <Footer />
           </MenuProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
