be8f14d7947a9d30f50f2e7dce6b2969d2d7c7fa
abrandonwang <abrandonwang@gmail.com>
2026-05-15 01:39:24 -0500

    fix brawlify cdn wrong urls

--- src/components/BrawlImage.tsx
+++ src/components/BrawlImage.tsx
@@ -8,13 +8,19 @@ type Props = Omit<ImageProps, "src" | "alt"> & {
 export function BrawlImage({ src, alt, sizes = "96px", ...props }: Props) {
   return (
     <Image
-      src={src}
+      src={normalizeBrawlAssetUrl(src)}
       alt={alt}
       sizes={sizes}
       unoptimized
       {...props}
     />
   )
+}
+
+export function normalizeBrawlAssetUrl(src: string) {
+  return src
+    .replace("cdn.brawlify.com/gadgets/borderless/", "cdn.brawlify.com/gadgets/regular/")
+    .replace("cdn.brawlify.com/star-powers/borderless/", "cdn.brawlify.com/star-powers/regular/")
 }
 
 export function brawlerIconUrl(id: number) {
